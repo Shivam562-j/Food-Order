0be5ba46dcb8a414de8e2d799c84bbbba6728ac2
shivam562-j <krshivam998@gmail.com>
2025-06-01 23:44:42 +0530

    make the mealitem component

---
+++ src/components/MealItem.jsx
@@ -0,0 +1,18 @@
+
+export default function MealItem({ meal = {} }) {
+    return (
+        <li className="meal-item">
+            <article>
+                <img src={`http://localhost:3000/${meal.image}`} alt={meal.name} />
+                <div>
+                    <h1>{meal.name}</h1>
+                    <p className="meal-item-price">{meal.price}</p>
+                    <p className="meal-item-description">{meal.description}</p>
+                </div>
+                <p className="meal-item-actions">
+                    <button>Add to Cart</button>
+                </p>
+            </article>
+        </li>
+    )
+}
