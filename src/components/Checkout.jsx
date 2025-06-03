567d979c49af8abf0e4827baa20e0ff7d72f4a28
shivam562-j <krshivam998@gmail.com>
2025-06-03 18:52:45 +0530

    modal esc button issue resolve and add form inthe checkout

--- src/components/Checkout.jsx
+++ src/components/Checkout.jsx
@@ -3,9 +3,13 @@ import { useContext } from "react";
 import Modal from "./UI/Modal";
 import CartContext from "../store/CartContext";
 import { currencyFormatter } from "../util/formatting";
+import Input from "./UI/Input";
+import Button from "./UI/Button";
+import UserProgressContext from "../store/UserProgressContext";
 
 export default function Checkout() {
     const cartCtx = useContext(CartContext);
+    const userProgressCtx = useContext(UserProgressContext);
 
     // handle total cart
     // const cartTotal = cartCtx?.items?.reduce(
@@ -21,10 +25,32 @@ export default function Checkout() {
         return totalPrice + validQuantity * validPrice;
     }, 0);
 
-    return <Modal>
+    // handle close checkout modal
+    function handleCloseCheckout() {
+        userProgressCtx.hideCheckout();
+    }
+
+    return <Modal
+        open={userProgressCtx?.progress === 'checkout'}
+        onClose={userProgressCtx?.progress === 'checkout' ? handleCloseCheckout : null}
+    >
         <form>
             <h2>Checkout</h2>
             <p>Total Amount: {currencyFormatter.format(cartTotal)}</p>
+
+            <Input label='Full Name' type='text' id='full-name' />
+            <Input label='E-Mail Address' type='email' id='email' />
+            <Input label='Street' type='text' id='street' />
+
+            <div className="control-row">
+                <Input label='Postal Code' type='text' id='postal-code' />
+                <Input label='City' type='text' id='city' />
+            </div>
+
+            <p className="modal-actions">
+                <Button type="button" textOnly onClick={handleCloseCheckout}>Close</Button>
+                <Button textOnly>Submit Order</Button>
+            </p>
         </form>
     </Modal>
 }
